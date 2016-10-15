7b6ebc9eec33340410304b7710de675510c7681e
PaulFranklin <pscf-jr@hotmail.com>
2016-10-14 21:55:24 -0500

    Excercise 4 Completed

--- maze.js
+++ maze.js
@@ -5,6 +5,7 @@ var endover =0;
 var start = document.getElementById('start');
 var boundaries = document.querySelectorAll('.boundary');
 var ending = document.getElementById('end');
+var rest= 
 
 start.onmouseover = function(){
 	check = 1;
@@ -18,24 +19,39 @@ for (var i = 0; i < boundaries.length; i++) {
 			
 		if (check === 1){
 		
-			for (var j = 0; j < boundaries.length; j++) {
+			for (var q = 0; q < boundaries.length; q++) {
 		  
-			boundaries[j].style.backgroundColor = "#ff8888";
-			}
+			boundaries[q].style.backgroundColor = "#ff8888";
 			}
 		}
-		}
-
+}
+}
+		
 
 ending.onmouseover = function(){
 	
  if (endover === 0 && check===1 )
 {
-	 console.log("hit")
-	alert("You Win Bruh");	
+	 alert("You Win Bruh");	
 }
 
 
 }
-}
 
+  
+      
+      
+     
+       start.addEventListener("click",function(){
+           
+          
+           for (var i = 0; i < boundaries.length; i++)
+               {
+                   boundaries[i].style.backgroundColor = "#eeeeee"; 
+           				   
+               }  
+           
+           
+           
+       });
+}
